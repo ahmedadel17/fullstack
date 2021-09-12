ed3e8afe82f754539ef04e003392189a45f8712e
ahmed elbastawesy <elbastawesy19@gmail.com>
2021-09-12 21:54:41 +0200

    a new edition

---
+++ site/main.js
@@ -0,0 +1,24 @@
+var welcome=document.getElementById("welcome");
+var fixed=document.getElementById("fixed");
+var second=document.getElementById("second");
+var second_c=document.getElementById("second_c");
+var dark=document.getElementById("dark");
+var darkmode=document.getElementById("darkmode");
+second_c.classList.add("displaynone");
+
+welcome.addEventListener("click",() => {
+    
+    fixed.classList.add("display");
+})
+second.addEventListener("click",() => 
+{
+    fixed.classList.add("displaynone");
+second_c.classList.add("display");
+
+    
+})
+darkmode.addEventListener("click",() => 
+{
+    dark.classList.toggle("darkmod");
+    
+})
