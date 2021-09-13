b204ec3eb5d50c380c525d46ee80b58dfa2212de
ahmed elbastawesy <elbastawesy19@gmail.com>
2021-09-13 18:57:17 +0200

    new card for html course

--- site/main.js
+++ site/main.js
@@ -4,22 +4,37 @@ var second=document.getElementById("second");
 var second_c=document.getElementById("second_c");
 var dark=document.getElementById("dark");
 var darkmode=document.getElementById("darkmode");
+var third=documnet.getElementById("third");
+var third_c=document.getElementById("third_c");
+third_c=document.getElementById("third_c");
+fixed.classList.add("displaynone");
 second_c.classList.add("displaynone");
+third_c.classList.add("displaynone");
+  welcome.addEventListener("click",() => {
+    
+      fixed.classList.add("display");
+      second_c.classList.add("displaynone");
+  third_c.classList.add("displaynone");
+  })
+  second.addEventListener("click",() => 
+  {
+      fixed.classList.add("displaynone");
+  second_c.classList.add("display");
+    second_c.classList.add("ratio ");
+    second_c.classList.add("ratio-16x9");
+    
+  })
+  third.addEventListener("click",() => 
+  {
+      fixed.classList.add("dislaynone");
+      second_c.classList.add("displaynone");
+      third_c.classList.add("display");
+      third_c.classList.add("displayfixed");
 
-welcome.addEventListener("click",() => {
     
-    fixed.classList.add("display");
-})
-second.addEventListener("click",() => 
-{
-    fixed.classList.add("displaynone");
-second_c.classList.add("display");
-// second_c.classList.add("ratio ");
-// second_c.classList.add("ratio-16x9");
+  })
+  darkmode.addEventListener("click",() => 
+  {
+      dark.classList.toggle("darkmod");
     
-})
-darkmode.addEventListener("click",() => 
-{
-    dark.classList.toggle("darkmod");
-    
-})
+  })
---
+++ site/new.js
@@ -0,0 +1,44 @@
+var first_c =document.getElementById("first_c");
+var second_c=document.getElementById("second_c");
+var third_c =document.getElementById("third_c");
+var first =document.getElementById("first");
+var second =document.getElementById("second");
+var third =document.getElementById("third");
+var learn =document.getElementById("learn");
+first_c.classList.add("displaynone");
+second_c.classList.add("displaynone");
+third_c.classList.add("displaynone");
+first.addEventListener("click",() => {
+    first_c.classList.remove("displaynone");
+    second_c.classList.add("displaynone");
+third_c.classList.add("displaynone");
+learn.classList.add("displaynone");
+    
+})
+second.addEventListener("click",() => {
+    second_c.classList.remove("displaynone");
+    first_c.classList.add("displaynone");
+    third_c.classList.add("displaynone");
+    learn.classList.add("displaynone");
+
+})
+third.addEventListener("click",() => {
+    third_c.classList.remove("displaynone");
+    second_c.classList.add("displaynone");
+    first_c.classList.add("displaynone");
+learn.classList.add("displaynone");
+
+})
+// first.addEventListener("click",() => {
+//     first_c.classList.add("display");
+//     second_c.classList.add("displaynone");
+//     third_c.classList.add("displaynone");
+    
+// })
+// third_c.addEventListener("click",() => 
+// {
+
+// second_c.classList.add("displaynone");
+// first_c.classList.add("displaynone");
+// third_c.classList.remove("displaynone");
+// })
